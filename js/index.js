673e58435804f67d4e4fd9cbf54d685b8f2049dd
alexahdp <alexahdp@gmail.com>
2018-06-19 23:28:26 +0300

    fix fitness function

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ const world = new World();
 const borders = world.createSteps();
 const agent = new Agent(world);
 agent.world = world;
+world.agent = agent;
 
 let keyPressed = false;
 document.addEventListener('keydown', e => {
@@ -22,44 +23,15 @@ document.addEventListener('keyup', e => {
 	agent.setVel(0);
 });
 
-
-function __computeFintess(world, agent) {
-	// надо:
-	// - выбрать нижнюю ступеньку
-	// - s0 = расстояние от проекции центра ступеньки на ось X агента на шаге i
-	// - s1 = расстояние от проекции центра ступеньки на ось X агента на шаге i + 1
-	// fitness = s1 > s0 ? 1 : -1
-	// как только ступенька опустилась ниже оси X - выбираем следующую ступеньку
-	
-	
-}
-
-
 function computeFintess(world, agent) {
-	// если агент столкнулся со ступенькой - наказание и перезапуск
-	// если ступенька достигла агента y агента и не пересекла по x - поощрение
-	
-	// +
-	// если глаза видят ступеньку и расстояние уменьшается - наказание
-	// если глаза видят ступеньку и расстояние увеличивается - поощрение
-	//const activeSensors = agent.sensors.filter(sensor => sensor);
-	
-	// это совершенно дурацкая fitness-функция
-	
 	const good = 1.0;
 	const bad = -1.0;
+	let res = good;
 	
-	let res = 0;
 	agent.vision.some((visionItem, i) => {
-		if (!agent.prevVision) return;
-		if (visionItem && agent.prevVision[i]) {
-			if (visionItem.distance <= agent.prevVision[i].distance) {
-				res = bad;
-				return true;
-			} else {
-				res = good;
-				return true;
-			}
+		if (visionItem && visionItem.distance < 10) {
+			res = bad;
+			return true;
 		}
 	});
 	
@@ -72,10 +44,14 @@ world.animate(() => {
 	const vision = agent.checkSensors();
 	const act = agent.getAction(vision);
 	
-	//console.log(agent.actionsMap[act])
 	agent.vel.x = agent.actionsMap[act];
 	agent.move();
-	world.moveSteps();
-	const fit = computeFintess(world, agent);
+	
+	// world.moveSteps();
+	// const fit = computeFintess(world, agent);
+	
+	const result = world.moveSteps();
+	const fit = result ? -1 : 0;
+	
 	agent.backward(fit);
 });
--- js/world.js
+++ js/world.js
@@ -34,7 +34,6 @@ class World {
 	}
 	
 	init() {
-		
 	}
 	
 	animate(cb) {
@@ -46,204 +45,10 @@ class World {
 	}
 	
 	render() {
-		//var time = Date.now() * 0.005;
-		// sphere.rotation.z = 0.01 * time;
-		// var geometry = sphere.geometry;
-		// var attributes = geometry.attributes;
-		
-		// for ( var i = 0; i < attributes.size.array.length; i++ ) {
-		// 	attributes.size.array[ i ] = 14 + 13 * Math.sin( 0.1 * i + time );
-		// }
-		
-		//attributes.size.needsUpdate = true;
 		this.renderer.render( this.scene, this.camera );
 	}
 	
-	// allocPoints() {
-	// 	var amount = 1;
-	// 	var radius = 10;
-		
-	// 	var positions = new Float32Array( amount * 3 );
-	// 	var colors = new Float32Array( amount * 3 );
-	// 	var sizes = new Float32Array( amount );
-		
-	// 	var vertex = new THREE.Vector3();
-	// 	var color = new THREE.Color( 0xffffff );
-		
-	// 	for ( var i = 0; i < amount; i ++ ) {
-	// 		vertex.x = ( Math.random() * 2 - 1 ) * radius;
-	// 		vertex.y = ( Math.random() * 2 - 1 ) * radius;
-	// 		vertex.z = ( Math.random() * 2 - 1 ) * radius;
-	// 		vertex.toArray( positions, i * 3 );
-			
-	// 		if ( vertex.x < 0 ) {
-	// 			color.setHSL( 0.5 + 0.1 * ( i / amount ), 0.7, 0.5 );
-	// 		} else {
-	// 			color.setHSL( 0.0 + 0.1 * ( i / amount ), 0.9, 0.5 );
-	// 		}
-			
-	// 		color.toArray( colors, i * 3 );
-	// 		sizes[ i ] = 10;
-	// 	}
-		
-	// 	var geometry = new THREE.BufferGeometry();
-	// 	geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-	// 	geometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
-	// 	geometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
-		
-	// 	var material = new THREE.ShaderMaterial( {
-	// 		uniforms: {
-	// 			amplitude: { value: 1.0 },
-	// 			color:     { value: new THREE.Color( 0xffffff ) },
-	// 			texture:   { value: new THREE.TextureLoader().load( "/sprites/spark1.png" ) }
-	// 		},
-	// 		vertexShader:   vertexshader,
-	// 		fragmentShader: fragmentshader,
-			
-	// 		blending:       THREE.AdditiveBlending,
-	// 		depthTest:      false,
-	// 		transparent:    true
-	// 	});
-		
-	// 	sphere = new THREE.Points( geometry, material );
-	// 	this.scene.add(sphere);
-	// }
-	
-	
-	// allocLines() {
-	// 	var segments = 10;
-		
-	// 	var geometry = new THREE.BufferGeometry();
-	// 	var material = new THREE.LineBasicMaterial({ vertexColors: THREE.VertexColors });
-		
-	// 	var positions = new Float32Array( segments * 3 );
-	// 	var colors = new Float32Array( segments * 3 );
-		
-	// 	var r = 100;
-		
-	// 	for ( var i = 0; i < segments; i ++ ) {
-	// 		var x = Math.random() * r - r / 2;
-	// 		var y = Math.random() * r - r / 2;
-	// 		var z = Math.random() * r - r / 2;
-			
-	// 		positions[ i * 3 ] = x;
-	// 		positions[ i * 3 + 1 ] = y;
-	// 		positions[ i * 3 + 2 ] = z;
-			
-	// 		colors[ i * 3 ] = ( x / r ) + 0.5;
-	// 		colors[ i * 3 + 1 ] = ( y / r ) + 0.5;
-	// 		colors[ i * 3 + 2 ] = ( z / r ) + 0.5;
-	// 	}
-		
-	// 	geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-	// 	geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
-	// 	geometry.computeBoundingSphere();
-	// 	const lines = new THREE.Line( geometry, material );
-		
-	// 	this.scene.add(lines);
-	// }
-	
 	createAgent() {
-		// var radius = 10;
-		
-		// var positions = new Float32Array( 3 );
-		// var colors = new Float32Array( 3 );
-		// var sizes = new Float32Array( 1 );
-		
-		// var vertex = new THREE.Vector3(0, 0, 0);
-		// var color = new THREE.Color( 0x34dd11 );
-		
-		// vertex.toArray( positions, 0 );
-		
-		// color.toArray( colors, 0 );
-		// sizes[0] = 10;
-		
-		// var geometry = new THREE.BufferGeometry();
-		// geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-		// geometry.addAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
-		// geometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
-		
-		// var material = new THREE.ShaderMaterial( {
-		// 	uniforms: {
-		// 		amplitude: { value: 1.0 },
-		// 		color:     { value: new THREE.Color( 0xffffff ) },
-		// 		texture:   { value: new THREE.TextureLoader().load( "/sprites/spark1.png" ) }
-		// 	},
-		// 	vertexShader:   vertexshader,
-		// 	fragmentShader: fragmentshader,
-			
-		// 	blending:       THREE.AdditiveBlending,
-		// 	depthTest:      false,
-		// 	transparent:    true
-		// });
-		
-		// const points = new THREE.Points( geometry, material );
-		// this.scene.add(points);
-		
-		// // ===================================
-		
-		// // var segments = 10;
-		
-		// // var geometry = new THREE.BufferGeometry();
-		// // var material = new THREE.LineBasicMaterial({ vertexColors: THREE.VertexColors });
-		
-		// // const nn = 3;
-		// // var positions = new Float32Array( segments * 3 );
-		// // var colors = new Float32Array( segments * 3 );
-		
-		// // var r = 100;
-		// // const c = [0, 0];
-		
-		
-		// // for ( let i = 0; i < segments; i ++ ) {
-		// // 	const [x, y] = t.crt2xy(c, 100,  i / (segments-1));
-		// // 	// var x = Math.random() * r - r / 2;
-		// // 	// var y = Math.random() * r - r / 2;
-		// // 	// var z = Math.random() * r - r / 2;
-		// // 	const z = 0
-		// // 	positions[ i * nn + 0 ] = x;
-		// // 	positions[ i * nn + 1 ] = y;
-		// // 	positions[ i * nn + 2 ] = z;
-		// // 	// positions[ i * nn + 3 ] = 0;
-		// // 	// positions[ i * nn + 4 ] = 0;
-		// // 	// positions[ i * nn + 5 ] = 0;
-			
-		// // 	// colors[ i * 3 ] = 0;
-		// // 	// colors[ i * 3 ] = 0;
-		// // 	// colors[ i * 3 ] = 0;
-		// // 	colors[ i * 3 ] = ( x / r ) + 0.5;
-		// // 	colors[ i * 3 + 1 ] = ( y / r ) + 0.5;
-		// // 	colors[ i * 3 + 2 ] = ( z / r ) + 0.5;
-		// // }
-		
-		// // geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-		// // geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
-		// // //geometry.computeBoundingSphere();
-		// // const lines = new THREE.Line( geometry, material );
-		
-		// // this.scene.add(lines);
-		
-		// // =======================
-		
-		// var segments = 10;
-		
-		// var geometry = new THREE.Geometry();
-		// var material = new THREE.LineBasicMaterial({ color: 0xffffff });
-		
-		// var r = VISION_DIST;
-		// const c = [0, 0];
-		// for ( let i = 0; i < segments; i ++ ) {
-		// 	const [x, y] = t.crt2xy(c, r,  i / (segments-1));
-		// 	geometry.vertices.push(
-		// 		new THREE.Vector3( 0, 0, 0 ),
-		// 		new THREE.Vector3( x, y, 0 ),
-		// 	);
-		// }
-		
-		// const lines = new THREE.LineSegments( geometry, material );
-		// this.scene.add(lines);
-		
-		// return {points, lines}
 	}
 	
 	getSide(y) {
@@ -310,18 +115,45 @@ class World {
 		// если ступень зашла слишком низко - поднимаем ее вверх
 		let minPointers = []
 		let maxVal = 0;
+		let collision = false;
 		
 		for (let i = 0; i < 10; i += 2) {
+			let above = this.agent.points.position.y <= this.steps.geometry.attributes.position.array[i * 3 + 1];
+			
 			this.steps.geometry.attributes.position.array[i * 3 + 1] -= 0.1;
 			this.steps.geometry.attributes.position.array[i * 3 + 4] -= 0.1;
+			
+			const agentPos = [
+				this.agent.points.position.x,
+				this.agent.points.position.y
+			];
+			
+			const stepPos = [
+				this.steps.geometry.attributes.position.array[i * 3 + 0],
+				this.steps.geometry.attributes.position.array[i * 3 + 1],
+				this.steps.geometry.attributes.position.array[i * 3 + 3],
+				this.steps.geometry.attributes.position.array[i * 3 + 4]
+			];
+			
+			let beyond = this.agent.points.position.y >= this.steps.geometry.attributes.position.array[i * 3 + 1];
+			
+			if (above && beyond && agentPos[0] >= stepPos[0] && agentPos[0] <= stepPos[2]) {
+				this.steps.geometry.attributes.position.array[i * 3 + 1] = 100;
+				this.steps.geometry.attributes.position.array[i * 3 + 4] = 100;
+				collision = true;
+				console.log('collision');
+			}
 			
 			if (this.steps.geometry.attributes.position.array[i * 3 + 1] < -100) {
 				this.steps.geometry.attributes.position.array[i * 3 + 1] = 100;
 				this.steps.geometry.attributes.position.array[i * 3 + 4] = 100;
+				console.log('omit');
 			}
 		}
 		
 		this.steps.geometry.attributes.position.needsUpdate = true;
+		
+		return collision;
 	}
 }
 
